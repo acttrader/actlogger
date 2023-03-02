db820b0d21b56880d80d661717dea8cfc5c114c4
alexey karavan <alex.karavan@gmail.com>
2023-03-02 15:33:23 +0200

    development

---
+++ actlogger.go
@@ -0,0 +1,52 @@
+package actlogger
+
+import (
+	"io"
+	"path"
+
+	"github.com/rs/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+type ActLogger struct {
+	*zerolog.Logger
+}
+
+type Config struct {
+	Directory  string
+	Filename   string
+	MaxSize    int  // MaxSize the max size in MB of the logfile before it's rolled
+	MaxBackups int  // MaxBackups the max number of rolled files to keep
+	MaxAge     int  // MaxAge the max age in days to keep a logfile
+	DebugLevel bool //DebugLevel bool
+	Compress   bool //Compress logs archive
+}
+
+// Configure sets up the logging framework
+func Configure(config Config) *ActLogger {
+	var writers []io.Writer
+
+	writers = append(writers, &lumberjack.Logger{
+		Filename:   path.Join(config.Directory, config.Filename),
+		MaxBackups: config.MaxBackups, // files
+		MaxSize:    config.MaxSize,    // megabytes
+		MaxAge:     config.MaxAge,     // days
+		Compress:   config.Compress,
+	})
+
+	zerolog.LevelFieldName = "level"
+	zerolog.TimestampFieldName = "time"
+
+	if config.DebugLevel {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	logger := zerolog.New(io.MultiWriter(writers...)).
+		With().
+		Timestamp().
+		Logger()
+
+	return &ActLogger{
+		Logger: &logger,
+	}
+}
